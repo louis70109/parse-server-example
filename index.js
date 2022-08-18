124d3c7aae0e56f9207cb573858e94077fccd4b1
NiJia Lin <louis70109@gmail.com>
2022-08-18 23:58:31 +0800

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const config = {
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse', // Don't forget to change to https if needed
-  restApiKey: process.env.REST_API_KEY || 'rest_API_key'
+  restApiKey: process.env.REST_API_KEY || 'rest_API_key',
   liveQuery: {
     classNames: ['Posts', 'Comments'], // List of classes to support for query subscriptions
   },
